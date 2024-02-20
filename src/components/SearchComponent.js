4733d915707e1ed4b73dd6840bb055222261085f
Muhammad Bilal Zahid Khan <muhammadbilalzahidkhan@gmail.com>
2024-02-20 16:35:22 +0500

    updated

---
+++ src/components/SearchComponent.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import colors from '../misc/GlobalStyles';
+
+const SearchComponent = ({ searchText, handleSearch }) => {
+  return (
+    <View style={styles.searchContainer}>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Search..."
+        onChangeText={handleSearch}
+        value={searchText}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    marginTop: 20,
+    paddingHorizontal: 20,
+    width: '90%',
+    alignSelf: 'center',
+    borderColor: colors.PRIMARY,
+    borderWidth: 2,
+    borderRadius: 10,
+  },
+  searchInput: {
+    height: 40,
+    fontSize: 16,
+  },
+});
+
+export default SearchComponent;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import colors from "../misc/GlobalStyles";
+import colors from '../misc/GlobalStyles';
+import SearchComponent from '../components/SearchComponent';
 
 const HomeScreen = () => {
   const currentHour = new Date().getHours();
@@ -14,13 +15,22 @@ const HomeScreen = () => {
     greeting = 'Good Evening';
   }
 
-  const name = 'Muhammad Bilal Zahid Khan'; 
+  const name = 'Muhammad Bilal Zahid Khan';
+
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearch = (text) => {
+    setSearchText(text);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.greetingContainer}>
-        <Text style={styles.greetingText}>{greeting}, {name}</Text>
+        <Text style={styles.greetingText}>
+          {greeting}, {name}
+        </Text>
       </View>
+      <SearchComponent searchText={searchText} handleSearch={handleSearch} />{/* Use the SearchComponent */}
     </View>
   );
 };
@@ -33,7 +43,7 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   greetingContainer: {
-    marginBottom: 20, 
+    marginBottom: 20,
     marginTop: 30,
     backgroundColor: colors.PRIMARY,
     marginHorizontal: 20,
