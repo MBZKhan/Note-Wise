0507fb171dbbea7ddf632de7116b78921a1f7da8
Muhammad Bilal Zahid Khan <muhammadbilalzahidkhan@gmail.com>
2024-02-21 17:32:47 +0500

    updated

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "../screens/HomeScreen";
 import IntroScreen from "../screens/IntroScreen";
+import NoteDetailsScreen from "../screens/NoteDetailsScreen";
 
 const Stack = createStackNavigator();
 
@@ -10,16 +11,21 @@ const AppNavigator = () => {
     return (
       <NavigationContainer>
         <Stack.Navigator>
-            <Stack.Screen
-                name="HomeScreen"
-                component={HomeScreen}
-                options={{ headerShown: false }}
-            />
             {/* <Stack.Screen
                 name="IntroScreen"
                 component={IntroScreen}
                 options={{ headerShown: false }}
             /> */}
+            {/* <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+                options={{ headerShown: false }}
+            /> */}
+            <Stack.Screen
+                name="NoteDetailsScreen"
+                component={NoteDetailsScreen}
+                options={{ headerShown: false }}
+            />
         </Stack.Navigator>
       </NavigationContainer>
     );
